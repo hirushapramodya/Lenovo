7ffeb204e7d2272ce6298cd129293c7946ad78fa
hirushapramodya1 <hirushapramodya234@gmail.com>
2024-08-12 19:28:07 +0530

    Added/test.java

---
+++ test1.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+public class test1 {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList();
+        arr.add(12);
+        arr.add(13);
+        arr.add(14);
+        System.out.println(arr);
+    }
+}
