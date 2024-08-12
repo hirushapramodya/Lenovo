df1cd4fde3b85d3c4c8ddc09b9cde06d0a8e91b8
hirushapramodya1 <hirushapramodya234@gmail.com>
2024-08-12 19:23:49 +0530

    Edded/test.java

--- test.java
+++ test.java
@@ -1,11 +1,18 @@
 class a {
     private String name;
+    private String village;
     private int age;
     public void setName(String name){
         this.name=name;
     }
     public void setAge(int age){
         this.age = age;
+    }
+    public void setVillage(String village){
+        this.village = village;
+    }
+    public String getVillage(){
+        return village;
     }
     public String getName(){
         return name;
@@ -21,6 +28,8 @@ public class test{
         System.out.println(obj.getAge());
         obj.setName("hbehuey");
         System.out.println(obj.getName());
+        obj.setVillage("kaluthara");
+        System.out.println(obj.getVillage());
     }
 
     
